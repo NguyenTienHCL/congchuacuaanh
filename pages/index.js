b4c272d2f26d6ec3f03029ce1ca5b3dfd058df93
NguyenTienHCL <101309897+NguyenTienHCL@users.noreply.github.com>
2024-05-13 15:34:53 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,14 +27,14 @@ export default function Home()
     
       <div className="flex flex-row justify-center items-center w-full h-full bg-pink-200 p-10 drop-shadow-xl">
         <div className="w-2/3 h-64 bg-gradient-to-r from-pink-300 to-pink-200 drop-shadow-xl rounded-full text-center transition-all duration-300">
-          <h1 className="p-20 tracking-wide text-8xl font-bold text-white uppercase drop-shadow-lg">Hi Beautiful</h1>
+          <h1 className="p-20 tracking-wide text-6xl font-bold text-white uppercase drop-shadow-lg">Hi Cong Chua!!!!!</h1>
         </div>
       </div>
 
       <div className="flex flex-row items-center justify-evenly w-full h-96 bg-pink-50">
-        <CommentedImage src='/marina1.jpg' text="wow so pretty"/>
+        <CommentedImage src='/marina1.jpg' text="u're so pretty"/>
         <CommentedImage src='/marina2.jpg' text="omg we&apos;re so cute"/>
-        <CommentedImage src='/marina3.jpg' text="we should kiss?"/>
+        <CommentedImage src='/marina3.jpg' text="we should kiss???"/>
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
@@ -45,17 +45,17 @@ export default function Home()
         <div className="flex flex-row items-center text-center w-full h-5/6 bg-pink-50">
           <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>You Need</h1>
           <img src='https://sweezy-cursors.com/wp-content/uploads/cursor/milk-amp-mocha-bear-love/milk-mocha-bear-love-custom-cursor.png' width={600}/>
-          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>Someone For</h1>
+          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>Someone Like</h1>
         </div>
-        <h1 className="w-full h-1/4 text-pink-300 font-bold text-6xl uppercase">prom</h1>
+        <h1 className="w-full h-1/4 text-pink-300 font-bold text-6xl uppercase">A guy makes you feel better</h1>
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
-        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">So...</h1>
+        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">...</h1>
       </div>
 
       <CornyQuestion 
-        question="Would you be the Elephant&apos;s Foot to my Chernobyl Disaster?"
+        question="Even though I made a very, very big mistake :(("
         nonactive="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuclear_symbol.svg/1024px-Nuclear_symbol.svg.png"
         active="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/76629/nuclear-explosion-clipart-xl.png"
         primary
@@ -67,35 +67,35 @@ export default function Home()
       </div>
 
       <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-pink-50 p-5">
-        <h1 className='w-2/3 text-pink-300 font-bold text-6xl'>Uh</h1>
+        <h1 className='w-2/3 text-pink-300 font-bold text-6xl'>...</h1>
       </div>
 
       <CornyQuestion 
-        question="Would you be the butt to my Red Guy?"
-        nonactive="https://loudbooru.com/_images/a1c2fa54153d30da1045a967fe8fd8a9/9228%20-%20character%3Alincoln_loud%20character%3Athe_red_guy%20cow_and_chicken%20crossover%20devil%20dialogue%20satan.png"
-        active="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/860bb581-65ee-4112-854f-1255c5579143/dffb3t8-d1a0991f-db5c-49ff-a7f2-2bc8d3278004.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg2MGJiNTgxLTY1ZWUtNDExMi04NTRmLTEyNTVjNTU3OTE0M1wvZGZmYjN0OC1kMWEwOTkxZi1kYjVjLTQ5ZmYtYTdmMi0yYmM4ZDMyNzgwMDQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.F2pBVw7v1HfA3zLlC5V5lc1xberJTFDwLT_GoKlT7Vw"
+        question="Dear My Princess, will you give me a chance to love and pamper you? :3"
+        nonactive="https://s.widget-club.com/images/YyiR86zpwIMIfrCZoSs4ulVD9RF3/d9d8dbbf46686c503874e3309c60d8b3/11b21e67cf06dfba76e8e912fe0aa96e.jpg?q=70&w=768"
+        active="https://i.pinimg.com/564x/aa/4e/37/aa4e371efdec17fccb9e3e7cea7b3871.jpg"
         secondary
         right
       />
 
       <CornyQuestion 
-        question="Would you be the smut to my Among Us?"
-        nonactive="https://assets.stickpng.com/images/61d183263a856e0004c6334a.png"
-        active="https://pbs.twimg.com/media/E2ieswGVgAYKWt3.jpg"
+        question="Will you still be my little princess?"
+        nonactive="https://i.pinimg.com/564x/74/68/6c/74686c6924f74e2a5b6e7128ffee874b.jpg"
+        active="https://i.pinimg.com/564x/7e/29/31/7e29318aedd6af055b77cd1b8b95fe58.jpg"
         primary
         left
       />
 
       <CornyQuestion 
         question="Would you be the banana to my Minion?"
-        nonactive="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a76b087-d923-4acf-85c5-118e6b81c7a0/d70lfsz-0a567aaf-a3ec-4cca-ac87-3820e4727e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzZiMDg3LWQ5MjMtNGFjZi04NWM1LTExOGU2YjgxYzdhMFwvZDcwbGZzei0wYTU2N2FhZi1hM2VjLTRjY2EtYWM4Ny0zODIwZTQ3MjdlOGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PZQsxdrOyzYABwsLATW4K94fX8RQBzUIzLrR-l8BDPU"
-        active="https://i.redd.it/v0ep5v1vbl331.png"
+        nonactive="https://i.pinimg.com/564x/4a/7e/5c/4a7e5c750f0e426ca592eaa8291520d1.jpg"
+        active="https://i.pinimg.com/564x/a8/0d/80/a80d809cf72d16cb75bdc4bc085175b4.jpg"
         secondary
         right
       />
 
       <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-pink-50 px-5 pt-5">
-        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>If I&apos;m C would you be my C++ to prom?</h1>
+        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>I really want to hear you nagging all day long, would you want to receive the honor I give you, my princess?</h1>
       </div>
 
       <div className="flex flex-row justify-around items-start text-center w-full h-36 bg-pink-50 pb-5">
@@ -159,28 +159,28 @@ export default function Home()
               Thank You For Your Confirmation!
             </h1>
             <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
-              Name of Partner
+              Name of Princess
             </h2>
             <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
-              Aidan Ouckama
+              Nguyen Phuong My Ha
             </h3>
             <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
               Location
             </h2>
             <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
-              Grove by the River, Newark
+              Dong Co, Tay Ho
             </h3>
             <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
-              Time
+              The level of cuteness
             </h2>
             <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
-              9:00 PM - 11:00 PM
+              100%
             </h3>
             <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
               Events
             </h2>
             <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
-              • Dinner • Photos • Prom • Post-Prom •
+              • Dinner •
             </h3>
             <img className="h-24" src="https://cdn.pixabay.com/photo/2018/02/19/20/28/abstract-3166168__480.png" alt=""/>
           </div>
